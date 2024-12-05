a19bc4e3b855049338c6e3529a128f137e027f88
ditchday <ditchday2025@gmail.com>
2024-12-04 23:13:56 -0800

    test2

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ var i = 0;
 var txt = 'Who are you, what are you, why are you'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
+var helpCounter = 0;
+var now;
+var timetilnexthint;
+
+
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("test").innerHTML = txt.substring(0, i+1) +'<span aria-hidden="true"></span>';
     i++;
     setTimeout(typeWriter, speed);
+  }
+}
+function help() {
+  now = new Date();
+  timetilnexthint = new Date(2024, 12, 5, 23, 15, 0, 0) - now;
+  if (timetilnexthint > 0) {
+    document.getElementById("hint").innerHTML = 'Now'; 
   }
 }
 typeWriter();
