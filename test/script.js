6486c54ef9cf54b94654eef5aafb3d9b600d3df2
ditchday <ditchday2025@gmail.com>
2024-12-04 22:38:32 -0800

    test

---
+++ test/script.js
@@ -0,0 +1,12 @@
+var i = 0;
+var txt = 'Who are you, what are you, why are you'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("test").innerHTML = txt.substring(0, i+1) +'<span aria-hidden="true"></span>';
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
