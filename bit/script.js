cc83ef84c7eadb3c3f98e86c37a713d5bc03f8f4
ditchday <ditchday2025@gmail.com>
2024-12-05 06:52:38 -0800

    test2

---
+++ bit/script.js
@@ -0,0 +1,73 @@
+var i = 0;
+var txt = '&#x202e;Binary to letter encoder, put in 5 bits and try it out'; /* The text */
+var stl = 'As we know, the internet is huge, and thus nothing is original anymore. Might as well give into it. Recreate the following photo and send it in spam. We will give you a code if its good.'
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+var hint1 = 'Hint: Top right is lame, I prefer bottom left';
+var hint2 = 'Hint: Where have you seen bits';
+var hint3 = 'Hint: Go to the wallboard. See the bits? Read it starting from the bottom left and use the converter.';
+
+var helpCounter = 0;
+var input;
+var aaa;
+
+var now;
+
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("test").innerHTML = txt.substring(0, i+1) +'<span aria-hidden="true"></span>';
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+  else {
+    document.getElementById("test").innerHTML = txt + '<br><input type="text" id="input" required minlength="5" maxlength="5"><span id="output"></span><br><button onclick="convert();">&#x202e;Convert</button><br><button onclick="help();">Hint</button>'
+  }
+}
+
+function convert() {
+  input = document.getElementById("input");
+  aaa = parseInt(input.value, 2)
+  if (aaa == 0) {
+    document.getElementById("output").innerHTML = "_";
+  }
+  else if (aaa>0 && aaa<27)
+    document.getElementById("output").innerHTML = String.fromCharCode(64+aaa);
+  else {
+    document.getElementById("output").innerHTML = "Invalid";
+  }
+}
+
+function help() {
+  if (helpCounter == 0) {
+    document.getElementById("test").innerHTML = txt + '<br><input type="text" id="input" required minlength="5" maxlength="5"><span id="output"></span><br><button onclick="convert();">&#x202e;Convert</button><br><button onclick="help();">Hint</button><br>'+hint1+"<br>Punishment: Record yourself doing part of the Cha Cha slide";
+    helpCounter++;
+  }
+  else if (helpCounter == 1) {
+    document.getElementById("test").innerHTML = txt + '<br><input type="text" id="input" required minlength="5" maxlength="5"><span id="output"></span><br><button onclick="convert();">&#x202e;Convert</button><br><button onclick="help();">Hint</button><br>'+hint2+"<br>Punishment: Give a senior a complement in binary in spam";
+    helpCounter++;
+  }
+  else if (helpCounter == 2) {
+    document.getElementById("test").innerHTML = txt + '<br><input type="text" id="input" required minlength="5" maxlength="5"><span id="output"></span><br><button onclick="convert();">&#x202e;Convert</button><br><button onclick="help();">Hint</button><br>'+hint3+"<br>Punishment: Convert your energy into productive means. 10 pushups, all of you; send in spam";
+    helpCounter++;
+  }
+}
+
+function stall() {
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("test").innerHTML = txt.substring(0, i+1) +'<span aria-hidden="true"></span>';
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+    else {
+      document.getElementById("test").innerHTML = txt + '<br><input type="text" id="input" required minlength="5" maxlength="5"><span id="output"></span><br><button onclick="convert();">&#x202e;Convert</button><br><button onclick="help();">Hint</button>'
+    }
+  }
+}
+var timetobegin = new Date('2024-12-05T08:35:00') - now;
+if (timetobegin < 0) {
+  typeWriter();
+}
+else {
+  stall();
+}
--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var i = 0;
-var txt = 'Who are you, what are you, why are you'; /* The text */
+var txt = 'Welcome to the internet! Everything you need is here, you just need where to look. If you are truly confused, we got you... for a price'; /* The text */
+var early = 'You are too early, come back at 8'
 var speed = 50; /* The speed/duration of the effect in milliseconds */
+var hint1 = 'Hint: See what is unseen';
+var hint2 = 'Hint: Have you tried highlighting everything?';
+var hint3 = 'Hint: Bro its just hidden text, whatever its bike rack go to the bike rack';
 
 var helpCounter = 0;
 var now;
@@ -19,8 +23,49 @@ function typeWriter() {
 }
 function help() {
   now = Date.now();
-  timetilnexthint = new Date('2024-12-05T00:44:00') - now;
-  document.getElementById("hint").innerHTML = timetilnexthint; 
-
+  if (helpCounter == 0) {
+    timetilnexthint = new Date('2024-12-08T05:05:00') - now;
+    if (timetilnexthint < 0) {
+      document.getElementById("test").innerHTML = txt + '<br><span class="hid">bike rack</span><span aria-hidden="true"></span><br><button onclick="help();">Hint</button><br>'+hint1+"<br>Punishment: Put on the sunglasses behind the wallboard and keep them on for the whole stack";
+      helpCounter++;
+    }
+    else {
+    	document.getElementById("test").innerHTML = document.getElementById("test").innerHTML + "<br>Wait a bit for the hint.";
+    }
+  }
+  else if (helpCounter == 1) {
+    timetilnexthint = new Date('2024-12-08T05:06:00') - now;
+    if (timetilnexthint < 0) {
+      document.getElementById("test").innerHTML = txt + '<br><span class="hid">bike rack</span><span aria-hidden="true"></span><br><button onclick="help();">Hint</button><br>'+hint2+"<br>Punishment: Use the highlighter and highlight yourself. Go wild.";
+      helpCounter++;
+    }
+    else {
+    	document.getElementById("test").innerHTML = document.getElementById("test").innerHTML + "<br>Wait a bit for the hint.";
+    }
+  }
+  else if (helpCounter == 2) {
+    timetilnexthint = new Date('2024-12-08T05:07:00') - now;
+    if (timetilnexthint < 0) {
+      document.getElementById("test").innerHTML = txt + '<br><span class="hid">bike rack</span><span aria-hidden="true"></span><br><button onclick="help();">Hint</button><br>'+hint3+"<br>Punishment: You make me sad. Everyone send the sad emoji in spam";
+      helpCounter++;
+    }
+    else {
+    	document.getElementById("test").innerHTML = document.getElementById("test").innerHTML + "<br>Wait a bit for the hint.";
+    }
+  }
 }
-typeWriter();
+function tooearly() {
+  if (i < txt.length) {
+    document.getElementById("test").innerHTML = early.substring(0, i+1) +'<span aria-hidden="true"></span>';
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+now = Date.now()
+var timetobegin = new Date('2024-12-05T08:00:00') - now
+if (timetobegin < 0) {
+  typeWriter();
+}
+else {
+  tooearly();
+}
