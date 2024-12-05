1a06d0f116533d884c57c02258ad14ac713cd269
ditchday <ditchday2025@gmail.com>
2024-12-04 23:40:14 -0800

    test2

--- script.js
+++ script.js
@@ -13,10 +13,13 @@ function typeWriter() {
     i++;
     setTimeout(typeWriter, speed);
   }
+  else {
+    document.getElementById("test").innerHTML = txt + '<span aria-hidden="true"></span>' + '<button onclick="myFunction();">My Title</button>'
+  }
 }
 function help() {
   now = new Date();
-  timetilnexthint = new Date(2024, 12, 5, 23, 30, 0, 0) - now;
+  timetilnexthint = new Date(2024, 12, 5, 23, 50, 0, 0) - now;
   if (timetilnexthint > 0) {
     document.getElementById("hint").innerHTML = 'Now'; 
   }
