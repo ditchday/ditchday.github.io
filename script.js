2c9d440d048246e482f528d2ca406e1ac97b8166
ditchday <ditchday2025@gmail.com>
2024-12-05 00:00:17 -0800

    test

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
   else {
-    document.getElementById("test").innerHTML = txt + '<span aria-hidden="true"></span>' + '<button onclick="myFunction();">My Title</button>'
+    document.getElementById("test").innerHTML = txt + '<span aria-hidden="true"></span>' + '<br><button onclick="help();">Hint</button>'
   }
 }
 function help() {
   now = new Date();
-  timetilnexthint = new Date(2024, 12, 5, 23, 50, 0, 0) - now;
+  timetilnexthint = new Date(2024, 12, 6, 0, 20, 0, 0) - now;
   if (timetilnexthint > 0) {
     document.getElementById("hint").innerHTML = 'Now'; 
   }
